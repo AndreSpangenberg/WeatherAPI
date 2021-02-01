5f2a91d6cc816f10a1fc310a2e100a925e4208fc
AndreSpangenberg <spangenberg6227@gmail.com>
2021-02-01 17:58:41 +0200

    Changed react.component to component

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { Component } from "react";
 import apiKeys from "../api/apiKeys";
 import loader from "../image/WeatherIcons.gif";
 import ReactAnimatedWeather from "react-animated-weather";
 
-class Weather extends React.Component {
+class Weather extends Component {
   state = {
     lat: undefined,
     lon: undefined,
@@ -18,13 +18,13 @@ class Weather extends React.Component {
     sunrise: undefined,
     sunset: undefined,
     errorMsg: undefined,
-  };
+  }
 
   getPosition = () => {
     return new Promise(function (resolve, reject) {
       navigator.geolocation.getCurrentPosition(resolve, reject);
-    });
-  };
+    })
+  }
   getWeather = async (latitude, longitude) => {
     const api_call = await fetch(
       `${apiKeys.base}weather?lat=${latitude}&lon=${longitude}&units=metric&APPID=${apiKeys.key}`);
@@ -39,7 +39,7 @@ class Weather extends React.Component {
         humidity: data.main.humidity,
         main: data.weather[0].main,
         country: data.sys.country,
-      });
+      })
 
     switch (this.state.main) {
       case "Haze":
@@ -72,7 +72,7 @@ class Weather extends React.Component {
       default:
         this.setState({ icon: "CLEAR_DAY" });
     }
-  };
+  }
   componentDidMount() {
     this.getPosition()
     .then((position) => {
