99ac3e2242fda1822c1823d9dae50f94caddd0bd
AndreSpangenberg <spangenberg6227@gmail.com>
2021-02-03 15:10:43 +0200

    Applied react sticky

--- src/components/HomeSection.js
+++ src/components/HomeSection.js
@@ -3,16 +3,15 @@ import '../App.css';
 import './HomeSection.css';
 import { Button } from './Button';
 
-
 function HomeSection() {
     return (
         <div className = 'home-container'>
-          <h1>GET YOUR WEATHER FORECAST</h1>
-          <div className="home-btn">
-              <Button className="btn" buttonStyle='btn--outline' 
-              buttonSize='btn--large' >Get Forecast</Button>
-          </div>
+        <h1>GET YOUR WEATHER FORECAST</h1>
+        <div className="home-btn">
+            <Button className="btn" buttonStyle='btn--outline' 
+            buttonSize='btn--large' >Get Forecast</Button>
         </div>
+      </div>
     );
 }
 
